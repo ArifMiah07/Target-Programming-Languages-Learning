cbad85a1a6c73418a7845b46a21cbf6af4357a44
ArifMiah07 <arifmiah.me322@gmail.com>
2024-10-30 18:48:50 +0600

    added python file

---
+++ src/python/code/files/calculator.py
@@ -0,0 +1,18 @@
+# one = 1
+# two = 2
+# three = 3
+# four = 4
+# five = 5
+# # six = 6
+# seven = 7
+
+a = int(input("a : "))
+b = int(input("b : "))
+c = int(input("c : "))
+
+num  = 3
+sum = a+b+c
+
+avg = sum / num 
+
+print(f"avg: {avg}")
